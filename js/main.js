98d8d816fb945849197b5a9216b010f4649a7984
wayou <liuwayong@gmail.com>
2017-06-11 02:48:04 +0000

    site updated via travis-ci at 2017-06-11 02:48:04 65ed94756d375fe4715a454256385ba5c6e2f10a

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-tocbot.init({tocSelector:".js-toc",contentSelector:".post-content",headingSelector:"h2, h3, h4",positionFixedSelector:".js-toc",headingsOffset:500});Waves.attach(".pager-item,a.page-number,.ink");Waves.init();
+if(document.querySelector(".js-toc")){tocbot.init({tocSelector:".js-toc",contentSelector:".post-content",headingSelector:"h2, h3, h4",positionFixedSelector:".js-toc",listItemClass:"toc-list-item truncate-text"})}Waves.attach(".pager-item,a.page-number,.ink");Waves.init();
