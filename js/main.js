e8f20c3c121b4462f3be004a18a79a9f1f1092c5
wayou <liuwayong@gmail.com>
2017-06-17 03:15:51 +0000

    site updated via travis-ci at 2017-06-17 03:15:51 6d210cba088ce42fa0aea6da7a1bbf913a0882ef

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-if(document.querySelector(".js-toc")){tocbot.init({tocSelector:".js-toc",contentSelector:".post-content",headingSelector:"h2, h3, h4",positionFixedSelector:".js-toc",listItemClass:"toc-list-item truncate-text"})}Waves.attach(".pager-item,a.page-number,.ink");Waves.init();
+
+if (document.querySelector('.js-toc')) {
+    tocbot.init({
+        tocSelector: ".js-toc",
+        contentSelector: ".post-content",
+        headingSelector: "h2, h3, h4",
+        positionFixedSelector: ".js-toc",
+        listItemClass: 'toc-list-item truncate-text'
+    })
+}
+
+Waves.attach('.pager-item,a.page-number,.ink')
+Waves.init()
+
+Array.prototype.map.call(document.querySelectorAll('.post-content img'), function (ele, i) {
+    ele.onclick = function (e) {
+        window.open(e.currentTarget.src, '_blank')
+    }
+})
