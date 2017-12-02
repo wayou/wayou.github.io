c3b7a1be7b673df803682dde5034cf7159d45cd3
wayou <liuwayong@gmail.com>
2017-12-02 15:35:19 +0800

    new post: js-exception-report

---
+++ js-exception-report/src/reportLib.js
@@ -0,0 +1,322 @@
+/**
+ * @description 根据腾讯的监控在结合自身的场景改的
+ * @author duanjun
+ * @date 2016-02-01
+ */
+
+/*jshint browser: true, esversion: 6, vars: true, nomen: true, indent: 4, maxlen: 120, plusplus: true, sloppy: true*/
+/*global define: true, require, $ , module, angular, __inline, console, phantom*/
+
+/*!
+ * @module report
+ * @author kael, chriscai
+ * @date @DATE
+ * Copyright (c) 2014 kael, chriscai
+ * Licensed under the MIT license.
+ */
+var BJ_REPORT = (function(global) {
+    if (global.BJ_REPORT) return global.BJ_REPORT;
+
+    var _log_map = {};
+    var _error = [];
+    var defaultParam = {};
+    var _config = {
+        id: 0,
+        uin: 0,
+        repeat: 3,
+        url: '',
+        combo: 1,
+        ext: {},
+        level: 4, // 1-debug 2-info 4-error
+        ignore: [],
+        random: 1,
+        delay: 1000,
+        submit: null
+    };
+
+    var _isOBJByType = function(o, type) {
+        return Object.prototype.toString.call(o) === '[object ' + (type || 'Object') + ']';
+    };
+
+    var _isOBJ = function(obj) {
+        var type = typeof obj;
+        return type === 'object' && !!obj;
+    };
+
+    var _isEmpty = function(obj) {
+        if (obj === null) return true;
+        if (_isOBJByType(obj, 'Number')) {
+            return false;
+        }
+        return !obj;
+    };
+
+    var isRepeat = function(msg) {
+        var times = (_log_map[msg] = (parseInt(_log_map[msg], 10) || 0) + 1);
+        return times > _config.repeat;
+    };
+
+    var orgError = global.onerror;
+
+    // rewrite window.oerror
+    global.onerror = function(msg, url, line, col, error) {
+        var newMsg = msg;
+
+        if (error && error.stack) {
+            newMsg = _processStackMsg(error);
+        }
+
+        if (_isOBJByType(newMsg, 'Event')) {
+            newMsg += newMsg.type
+                ? '--' + newMsg.type + '--' + (newMsg.target ? newMsg.target.tagName + '::' + newMsg.target.src : '')
+                : '';
+        }
+
+        report.push({
+            msg: newMsg,
+            target: url,
+            rowNum: line,
+            colNum: col
+        });
+
+        _send();
+        orgError && orgError.apply(global, arguments);
+    };
+
+    var _processError = function(errObj) {
+        try {
+            if (errObj.stack) {
+                var url = errObj.stack.match('https?://[^\n]+');
+                url = url ? url[0] : '';
+                var rowCols = url.match(':(\\d+):(\\d+)');
+                if (!rowCols) {
+                    rowCols = [0, 0, 0];
+                }
+
+                var stack = _processStackMsg(errObj);
+                return {
+                    msg: stack,
+                    rowNum: rowCols[1],
+                    colNum: rowCols[2],
+                    target: url.replace(rowCols[0], '')
+                };
+            } else {
+                //ie 独有 error 对象信息，try-catch 捕获到错误信息传过来，造成没有msg
+                if (errObj.name && errObj.message && errObj.description) {
+                    return {
+                        msg: JSON.stringify(errObj)
+                    };
+                }
+                return errObj;
+            }
+        } catch (err) {
+            return errObj;
+        }
+    };
+
+    var _processStackMsg = function(error) {
+        var stack = error.stack
+            .replace(/\n/gi, '')
+            .split(/\bat\b/)
+            .slice(0, 5)
+            .join('@')
+            .replace(/\?[^:]+/gi, '');
+        var msg = error.toString();
+        if (stack.indexOf(msg) < 0) {
+            stack = msg + '@' + stack;
+        }
+        return stack;
+    };
+
+    var _error_tostring = function(error, index) {
+        var param = [];
+        var params = [];
+        var stringify = [];
+        if (_isOBJ(error)) {
+            error.level = error.level || _config.level;
+            for (var key in error) {
+                var value = error[key];
+                if (!_isEmpty(value)) {
+                    if (_isOBJ(value)) {
+                        try {
+                            value = JSON.stringify(value);
+                        } catch (err) {
+                            value = '[BJ_REPORT detect value stringify error] ' + err.toString();
+                        }
+                    }
+                    stringify.push(key + ':' + value);
+                    param.push(key + '=' + value);
+                    params.push(key + '[' + index + ']=' + value);
+                }
+            }
+        }
+
+        // msg[0]=msg&target[0]=target -- combo report
+        // msg:msg,target:target -- ignore
+        // msg=msg&target=target -- report with out combo
+        return [params.join('&'), stringify.join(','), param.join('&')];
+    };
+
+    var _imgs = [];
+    var _submit = function(bl_param) {
+        var url;
+        var _bl_param = {};
+        var id = parseInt(_config.id, 10);
+        if (!id) {
+            return;
+        }
+        _bl_param = {
+            id: id,
+            ext: JSON.stringify(_config.ext)
+        };
+        for (var key in bl_param) {
+            _bl_param[key] = bl_param[key];
+        }
+        var blParamString = JSON.stringify(_bl_param);
+
+        if (isRepeat(blParamString)) {
+            return;
+        }
+        var search = '?bl_param=' + encodeURIComponent(blParamString) + '&t=' + +new Date();
+        url = _config.url + search;
+
+        if (_config.submit) {
+            _config.submit(url);
+        } else {
+            var _img = new Image();
+            _imgs.push(_img);
+            _img.src = url;
+        }
+    };
+
+    var error_list = [];
+    var comboTimeout = 0;
+    var _send = function(isReoprtNow) {
+        while (_error.length) {
+            var isIgnore = false;
+            var error = _error.shift();
+            var error_str = _error_tostring(error, error_list.length);
+            if (_isOBJByType(_config.ignore, 'Array')) {
+                for (var i = 0, l = _config.ignore.length; i < l; i++) {
+                    var rule = _config.ignore[i];
+                    if (
+                        (_isOBJByType(rule, 'RegExp') && rule.test(error_str[1])) ||
+                        (_isOBJByType(rule, 'Function') && rule(error, error_str[1]))
+                    ) {
+                        isIgnore = true;
+                        break;
+                    }
+                }
+            }
+            if (!isIgnore) {
+                if (_config.combo) {
+                    error_list.push(error_str[0]);
+                } else {
+                    _submit({error: error_str[2]});
+                }
+                _config.onReport && _config.onReport(_config.id, error);
+            }
+        }
+
+        // 合并上报
+        var count = error_list.length;
+        if (count) {
+            var comboReport = function() {
+                clearTimeout(comboTimeout);
+                _submit({error: error_list.join('&') + '&count=' + count});
+                comboTimeout = 0;
+                error_list = [];
+            };
+
+            if (isReoprtNow) {
+                comboReport(); // 立即上报
+            } else if (!comboTimeout) {
+                comboTimeout = setTimeout(comboReport, _config.delay); // 延迟上报
+            }
+        }
+    };
+
+    var report = {
+        push: function(msg) {
+            // 将错误推到缓存池
+            if (Math.random() >= _config.random) {
+                return report;
+            }
+            _error.push(
+                _isOBJ(msg)
+                    ? _processError(msg)
+                    : {
+                          msg: msg
+                      }
+            );
+            _send();
+            return report;
+        },
+        report: function(msg) {
+            // error report
+            msg && report.push(msg);
+            _send(true);
+            return report;
+        },
+        info: function(msg) {
+            // info report
+            if (!msg) {
+                return report;
+            }
+            if (_isOBJ(msg)) {
+                msg.level = 2;
+            } else {
+                msg = {
+                    msg: msg,
+                    level: 2
+                };
+            }
+            report.push(msg);
+            return report;
+        },
+        debug: function(msg) {
+            // debug report
+            if (!msg) {
+                return report;
+            }
+            if (_isOBJ(msg)) {
+                msg.level = 1;
+            } else {
+                msg = {
+                    msg: msg,
+                    level: 1
+                };
+            }
+            report.push(msg);
+            return report;
+        },
+        init: function(config) {
+            // 初始化
+            if (_isOBJ(config)) {
+                for (var key in config) {
+                    _config[key] = config[key];
+                }
+            }
+
+            return report;
+        },
+
+        __onerror__: global.onerror
+    };
+
+    typeof console !== 'undefined' &&
+        console.error &&
+        setTimeout(function() {
+            var err = ((location.hash || '').match(/([#&])BJ_ERROR=([^&$]+)/) || [])[2];
+            err && console.error('BJ_ERROR', decodeURIComponent(err).replace(/(:\d+:\d+)\s*/g, '$1\n'));
+        }, 0);
+
+    return report;
+})(window);
+
+if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+        exports = module.exports = BJ_REPORT;
+    }
+    exports.BJ_REPORT = BJ_REPORT;
+}
